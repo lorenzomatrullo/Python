e33b3bae0b55f61422227dd5726977503d02f958
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-18 15:16:22 +0200

    type casting

---
+++ libs/typecasting.py
@@ -0,0 +1,11 @@
+# Typecasting = the proicess of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "My Name"
+age = 20
+price = 10.99
+is_developer = True
+print(type(price))   # display the data type of the variable
+
+price = int(price)
+print(type(price))
