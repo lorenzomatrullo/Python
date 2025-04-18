b096acbb1a8a347d0a733c01fac8294c920350ba
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-18 15:16:09 +0200

    data types

---
+++ libs/data_types.py
@@ -0,0 +1,25 @@
+
+
+# Strings
+name = "Lorenzo"
+food = "pizza"
+email = "lorenzo@test.com"
+
+# Integers
+age = 20
+quantity = 3
+
+print(f"You are {20} years old")
+print(f"You are buying {quantity} items")
+
+# Float
+price = 10.99
+gpa = 3.2
+
+print(f"The price is ${price}")
+print(f"Your gpa is: {gpa}")
+
+#Boolean
+is_developer = True
+
+print(f"Are you a student?: {is_developer}")
