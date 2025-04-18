fc935d6d774113006d173c63995d761108474e3f
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-18 20:21:22 +0200

    format specifiers

---
+++ libs/format_specifiers.py
@@ -0,0 +1,7 @@
+# format specifiers % = defines and formats a type of data to be displayed
+
+x = 3.14159265359
+
+print(x)
+print(round(x, 2))      # decimal precision (2 digits)
+print(round(x, 1))      # decimal precision (1 digit)
