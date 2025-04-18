5491191aeec646347c7a4d70d9c15635584b2f0f
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-18 15:15:57 +0200

    variables

---
+++ libs/variables.py
@@ -0,0 +1,11 @@
+# VARIABLE = Allocated space in memory to store a value. 
+# We refer to a variable's name to access the stored value.
+# That variable now behaves as if it was the value it contains.
+
+print("Hello World!")
+
+first_name = "Lorenzo"
+print(first_name)
+
+# f-string to call variables
+print(f"Hello {first_name}")
