14c4b2ba8004604fa3f3f60887a5e87c743f7b3f
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-18 20:04:29 +0200

    math operators

---
+++ libs/math_operators.py
@@ -0,0 +1,30 @@
+# Math Operators
+# 
+# +  (addition)
+# -  (subtraction)
+# *  (multiplication)
+# /  (division)
+# ** (exponentiation)
+# %  (modulus) means "the rest of a division"
+
+friends = 5
+
+# friends = friends + 1
+friends += 1
+
+# friends = friends - 2
+friends -= 2
+
+# friends = friends * 3
+friends *= 3
+
+# friends = friends / 2
+friends /= 2
+
+# friends = friends ** 2
+friends **= 2
+
+remainder = friends % 2
+
+print(friends)
+print(remainder)
