835757dae7d8127eb289939b1661c3e280b4b4ba
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-23 17:13:39 +0200

    ternary operators

---
+++ libs/ternary_operators.py
@@ -0,0 +1,9 @@
+# TERNARY OPERATORS = shortcut to if/else statement when assigning/returning a value
+
+age = 18
+status = "Adult" if age >= 18 else "Minor"
+print(status)
+
+user_role = "admin"
+access_level = "Full Access" if user_role == "admin" else "Limited Access"
+print(access_level)
