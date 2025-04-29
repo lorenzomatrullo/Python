093f9896c68e808c3e2a2378b7b5c69be6801dcf
Rel <147942029+lorenzomatrullo@users.noreply.github.com>
2025-04-29 11:25:20 +0200

    format specifiers

--- libs/format_specifiers.py
+++ libs/format_specifiers.py
@@ -1,7 +1,22 @@
-# format specifiers % = defines and formats a type of data to be displayed
+# format specifiers =  {value:flags} format a value based on what flags are inserted
 
-x = 3.14159265359
+price1 = 3.14159
+price2 = -987.65
+price3 = 1200.34
 
-print(x)
-print(round(x, 2))      # decimal precision (2 digits)
-print(round(x, 1))      # decimal precision (1 digit)
+print(f"Price 1 is ${price1:.2f}")       # decimal precision (2 digits)
+print(f"Price 1 is ${price1:.1f}")       # decimal precision (1 digit)
+
+print("\n")
+
+print(f"Price 1 is ${price1:10}")        # 10 spaces to display the output
+print(f"Price 1 is ${price1:<10}")       # left justified
+print(f"Price 1 is ${price1:>10}")       # right justified
+print(f"Price 1 is ${price1:^10}")       # center
+
+print("\n")
+
+print(f"Price 1 is ${price1:+}")         # prefix sign of the number
+print(f"Price 2 is ${price2:+}")         # prefix sign of the number
+print(f"Price 3 is ${price3:+}")         # prefix negative number
+print(f"Price 3 is ${price3:,}")         # separetes thousands place
