fec9eb0a907ae56c24261f248177a3d082ef49e4
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2025-05-09 15:04:34 +0200

    functions

---
+++ libs/functions.py
@@ -0,0 +1,14 @@
+# function = A block of reusable code
+#            place () after the function name to invoke it
+
+def happy_birthday():
+    print("Happy birhday to you!")
+
+
+def happy_birthday_toYou(name, age):
+    print(f"Happy birthday to {name}")
+    print(f"You are {age}  years old")
+
+happy_birthday()
+happy_birthday_toYou("Bro", 20)
+
